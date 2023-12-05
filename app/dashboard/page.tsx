9d139bf7d6769d3dc6af4c88d79c899095cb7429
DVGY <gaurav.gydv@gmail.com>
2023-12-05 23:00:56 +0530

    add json server for data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,10 @@
+import { Box } from '@/components/ui/box';
+
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
-  return <h1>Hello, Dashboard Page!</h1>;
+  return (
+    <Box className='dashboard-container'>
+      <Box className='welcome-usr-msg'></Box>
+    </Box>
+  );
 }
